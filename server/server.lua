417bf61389648ebef1b13a2c55c948bacc66b348
KevinGirardx <89563654+KevinGirardx@users.noreply.github.com>
2022-12-25 10:14:08 -0400

    Temp event swap till after christmas

--- server/server.lua
+++ server/server.lua
@@ -4,22 +4,20 @@ RegisterNetEvent('kevin-weaponscratch:scratchserial', function (data)
     local PlayerId = source
     local Player = QBCore.Functions.GetPlayer(PlayerId)
     local weapon = Player.Functions.GetItemByName(data.weapon)
+    print(data.slot)
     if weapon then
-        if weapon.info.isScratched then
+        print(data.serial)
+        if data.serial == 'Scratched' then
             TriggerClientEvent('QBCore:Notify', PlayerId, 'Serial already scratched..', 'error')
         else
             TriggerClientEvent('inventory:client:ItemBox', PlayerId,  QBCore.Shared.Items[weapon.name], 'remove')
-            local oldSerial = weapon.info.serie
-            local oldQuality = weapon.info.quality
-            local oldAmmo = weapon.info.ammo
             Player.Functions.RemoveItem(weapon.name, 1, data.slot)
             Wait(400)
 
             local info = {}
-            info.serie = oldSerial
-            info.isScratched = true
-            info.quality = oldQuality
-            info.ammo = oldAmmo
+            info.serie = 'Scratched'
+            info.quality = data.quality
+            info.ammo = data.ammo
             Player.Functions.AddItem(weapon.name, 1, data.slot, info)
             TriggerClientEvent('inventory:client:ItemBox', PlayerId,  QBCore.Shared.Items[weapon.name], 'add')
         end
