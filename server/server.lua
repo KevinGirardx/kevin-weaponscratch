5542427fdf36a8f8818c85c472db54c0e4c1ecc2
KevinGirardx <89563654+KevinGirardx@users.noreply.github.com>
2022-12-26 13:53:58 -0400

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,25 +1,19 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterNetEvent('kevin-weaponscratch:scratchserial', function (data)
-    local PlayerId = source
-    local Player = QBCore.Functions.GetPlayer(PlayerId)
-    local weapon = Player.Functions.GetItemByName(data.weapon)
-    print(data.slot)
-    if weapon then
-        print(data.serial)
-        if data.serial == 'Scratched' then
-            TriggerClientEvent('QBCore:Notify', PlayerId, 'Serial already scratched..', 'error')
-        else
-            TriggerClientEvent('inventory:client:ItemBox', PlayerId,  QBCore.Shared.Items[weapon.name], 'remove')
-            Player.Functions.RemoveItem(weapon.name, 1, data.slot)
-            Wait(400)
-
-            local info = {}
-            info.serie = 'Scratched'
-            info.quality = data.quality
-            info.ammo = data.ammo
-            Player.Functions.AddItem(weapon.name, 1, data.slot, info)
-            TriggerClientEvent('inventory:client:ItemBox', PlayerId,  QBCore.Shared.Items[weapon.name], 'add')
-        end
+RegisterNetEvent('kevin-weaponscratch:scratchserial', function(data)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local weapon = Player.Functions.GetItemBySlot(data.slot)
+    if weapon.name ~= data.weapon then return end
+    if weapon.serial ~= 'Scratched' then
+        if not Player.Functions.RemoveItem(weapon.name, 1, weapon.slot) then return end
+        TriggerClientEvent('inventory:client:ItemBox', src,  QBCore.Shared.Items[weapon.name], 'remove')
+        Wait(400)
+        local info = {}
+        info.serie = 'Scratched'
+        info.quality = weapon.quality
+        info.ammo = weapon.ammo
+        Player.Functions.AddItem(weapon.name, 1, weapon.slot, info)
+        TriggerClientEvent('inventory:client:ItemBox', src,  QBCore.Shared.Items[weapon.name], 'add')
     end
 end)
