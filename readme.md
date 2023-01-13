96c524bfe68ef1f0a522a107b15e47f40d71954f
KevinGirardx <89563654+KevinGirardx@users.noreply.github.com>
2023-01-13 11:18:12 -0400

    added item check

--- readme.md
+++ readme.md
@@ -4,6 +4,13 @@
 - qb-target
 - ox_lib https://github.com/overextended/ox_lib/releases/tag/v2.19.1
 
+* Add this to qb-core > shared > items.lua
+```lua
+
+['steelfile'] 				 		 = {['name'] = 'steelfile', 			  	  		['label'] = 'Steel File', 				['weight'] = 1000, 		['type'] = 'item', 		['image'] = 'steelfile.png', 				['unique'] = true, 		['useable'] = true, 	['shouldClose'] = true,	   ['combinable'] = nil,   ['description'] = '',				["created"] = nil,      ["decay"] = 60.0	},
+
+```
+
 # Credits
 - https://github.com/pushkart2
 - https://github.com/JoeSzymkowiczFiveM
--- server/server.lua
+++ server/server.lua
@@ -5,15 +5,20 @@ RegisterNetEvent('kevin-weaponscratch:scratchserial', function(data)
     local Player = QBCore.Functions.GetPlayer(src)
     local weapon = Player.Functions.GetItemBySlot(data.slot)
     if weapon.name ~= data.weapon then return end
-    if weapon.serial ~= 'Scratched' then
-        if not Player.Functions.RemoveItem(weapon.name, 1, weapon.slot) then return end
-        TriggerClientEvent('inventory:client:ItemBox', src,  QBCore.Shared.Items[weapon.name], 'remove')
-        Wait(400)
-        local info = {}
-        info.serie = 'Scratched'
-        info.quality = weapon.quality
-        info.ammo = weapon.ammo
-        Player.Functions.AddItem(weapon.name, 1, weapon.slot, info)
-        TriggerClientEvent('inventory:client:ItemBox', src,  QBCore.Shared.Items[weapon.name], 'add')
+    local Item = Player.Functions.GetItemByName('steelfile')
+    if Item then
+        if weapon.info.serial ~= 'Scratched' then
+            if not Player.Functions.RemoveItem(weapon.name, 1, weapon.slot) then return end
+            TriggerClientEvent('inventory:client:ItemBox', src,  QBCore.Shared.Items[weapon.name], 'remove')
+            Wait(400)
+            local info = {}
+            info.serie = 'Scratched'
+            info.quality = weapon.info.quality
+            info.ammo = weapon.info.ammo
+            Player.Functions.AddItem(weapon.name, 1, weapon.slot, info)
+            TriggerClientEvent('inventory:client:ItemBox', src,  QBCore.Shared.Items[weapon.name], 'add')
+        end
+    else
+        TriggerClientEvent("QBCore:Notify", src, 'You do not have any tools for this..', "error")
     end
 end)
