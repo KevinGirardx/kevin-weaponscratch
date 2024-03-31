973f345b00aa5f21f422bbcc6c11188d63fa753e
XDope <bhavik3133@gmail.com>
2024-03-31 09:06:42 -0700

    ox_inventory support

--- server/server.lua
+++ server/server.lua
@@ -1,24 +1,40 @@
 local QBCore = exports['qb-core']:GetCoreObject()
+local usingox = GetResourceState('ox_inventory') == 'started'
 
 RegisterNetEvent('kevin-weaponscratch:scratchserial', function(data)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    local weapon = Player.Functions.GetItemBySlot(data.slot)
-    if weapon.name ~= data.weapon then return end
-    local Item = Player.Functions.GetItemByName('steelfile')
-    if Item then
-        if weapon.info.serial ~= 'Scratched' then
-            if not Player.Functions.RemoveItem(weapon.name, 1, weapon.slot) then return end
-            TriggerClientEvent('inventory:client:ItemBox', src,  QBCore.Shared.Items[weapon.name], 'remove')
-            Wait(400)
-            local info = {}
-            info.serie = 'Scratched'
-            info.quality = weapon.info.quality
-            info.ammo = weapon.info.ammo
-            Player.Functions.AddItem(weapon.name, 1, weapon.slot, info)
-            TriggerClientEvent('inventory:client:ItemBox', src,  QBCore.Shared.Items[weapon.name], 'add')
+    if usingox then
+        local weapon = exports.ox_inventory:GetSlot(src, data.slot)
+        if weapon.name ~= data.weapon then return end
+        local Item = Player.Functions.GetItemByName('steelfile')
+        if Item then
+            if weapon.metadata.serial ~= 'Scratched' then
+                weapon.metadata.serial = 'Scratched'
+                exports.ox_inventory:SetMetadata(source, weapon.slot, weapon.metadata)
+                TriggerClientEvent("QBCore:Notify", src, 'Successfully scratched weapon serial number', "success")
+            end
+        else
+            TriggerClientEvent("QBCore:Notify", src, 'You do not have any tools for this..', "error")
         end
     else
-        TriggerClientEvent("QBCore:Notify", src, 'You do not have any tools for this..', "error")
+        local weapon = Player.Functions.GetItemBySlot(data.slot)
+        if weapon.name ~= data.weapon then return end
+        local Item = Player.Functions.GetItemByName('steelfile')
+        if Item then
+            if weapon.info.serial ~= 'Scratched' then
+                if not Player.Functions.RemoveItem(weapon.name, 1, weapon.slot) then return end
+                TriggerClientEvent('inventory:client:ItemBox', src,  QBCore.Shared.Items[weapon.name], 'remove')
+                Wait(400)
+                local info = {}
+                info.serie = 'Scratched'
+                info.quality = weapon.info.quality
+                info.ammo = weapon.info.ammo
+                Player.Functions.AddItem(weapon.name, 1, weapon.slot, info)
+                TriggerClientEvent('inventory:client:ItemBox', src,  QBCore.Shared.Items[weapon.name], 'add')
+            end
+        else
+            TriggerClientEvent("QBCore:Notify", src, 'You do not have any tools for this..', "error")
+        end
     end
 end)
